fa21e73f812b8b54f0812fe61715eefa879fd00f
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-11-14 04:57:17 +0000

    Fix generator args

--- chaste_codegen_sbml/generator.py
+++ chaste_codegen_sbml/generator.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from os.path import basename
-from libsbml import *
+from libsbml import SBMLReader
 from . import translator
 from . import filewriters
 
@@ -46,42 +46,30 @@ def MoveFilesToDirectory(files, new_path):
     for i in range(len(files)):
         os.rename(current_path + "/" + files[i], new_path + "/" + files[i])
 
-def Generate(path_to_file):
+def Generate(sbml_file, output_filename=None, output_directory=None):
     """ Main function. Arguments are:
     Input, Output Filename, Output Directory. """
 
-    # path_to_file = args[1]
-
     #Get the model from the file
     reader = SBMLReader()
-    document = reader.readSBMLFromFile(path_to_file)
+    document = reader.readSBMLFromFile(sbml_file)
     model = document.getModel()
 
     #Get Name of file
-    # if(len(args) > 2):
-    #     model_name = args[2]
-    # else:
-    #     filename = basename(path_to_file)
-    #     split_fname = filename.split('.')
-    #     model_name = split_fname[0]
-
-    filename = basename(path_to_file)
-    model_name = filename.split('.')[0]
+    if not output_filename:
+        output_filename = basename(sbml_file).split('.')[0]
 
     # If there are events in the model, we define the class as a cell cycle model.
     # Otherwise, the model is assumed to be a subcellular reaction network model.
-    if( translator.IsSrnModel(model) ):
-        filewriters.WriteSrnModelToFile(model_name, model)
+    if translator.IsSrnModel(model):
+        filewriters.WriteSrnModelToFile(output_filename, model)
     else:
-        filewriters.WriteCcmModelToFile(model_name, model)
+        filewriters.WriteCcmModelToFile(output_filename, model)
 
-    # if(len(args) > 3):
-    #     output_directory = args[3]
-    # else:
-    #     output_directory = "."
-    output_directory = "."
+    if not output_directory:
+        output_directory = "."
 
-    output_files = GetOutputFilenames(path_to_file, model_name)
+    output_files = GetOutputFilenames(sbml_file, output_filename)
 
     #Move created files if output directory has been specified.
     MoveFilesToDirectory(output_files, output_directory)
